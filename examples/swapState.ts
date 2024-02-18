8108eaa68bb99803978cc877e4c5c561608c33a8
jjfluid <constant-space@gmail.com>
2024-02-17 21:25:57 -0800

    added swap example

---
+++ examples/swapState.ts
@@ -0,0 +1,56 @@
+import {Tracer} from "../src";
+import * as util from "util";
+import {config} from "dotenv";
+import {MAINNET_UNISWAPV2_ROUTER, MAINNET_USDC_ADDRESS, MAINNET_WETH_ADDRESS, MAX_UINT_256} from "../src/lib/constants";
+import {TraceType} from "../src/lib/Tracer";
+
+const Web3Utils = require('web3-utils');
+
+const MY_DEV_ADDRESS: string = '0x6b8fA3E8E2FDABC3d9Cd5985Ee294aa44B82B351';
+const TEN_ETH = Web3Utils.toWei('10', 'ether');
+
+config();
+
+// you only need this if you are not using ssl for your nodes - shhhhhh
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = String(0);
+
+const jsonRpcUrl = process.env.JSON_RPC_URL as string;
+const tracer = new Tracer({jsonRpcUrl});
+
+(async () => {
+  // =========================================================================
+  //
+  // swapping a large amount of ETH to USDC
+  //
+  // =========================================================================
+
+  // encode a swap on UniswapV2 from 10 ETH -> USDC
+  const swapData = tracer.swapCallEncoder.encodeSwapExactEthForTokens(
+    '0',  // not worried about slippage - only tracing
+    [MAINNET_WETH_ADDRESS, MAINNET_USDC_ADDRESS],
+    MY_DEV_ADDRESS,
+    MAX_UINT_256,  // never expires
+  );
+
+  // prepare swap tx
+  const swapTx = {
+    from: MY_DEV_ADDRESS,
+    to: MAINNET_UNISWAPV2_ROUTER,
+    value: TEN_ETH,
+    data: swapData,
+  }
+
+  // trace swap tx and cache state
+  const result0 = await tracer.traceCall({...swapTx}, {
+    traceType: TraceType.state,
+    balanceOverride: TEN_ETH,
+    useCachedState: false,
+    cacheStateFromTrace: true,
+  });
+
+  console.log('\nEthCallFetcher result for swap from ETH -> USDC:');
+  console.log('===========================================================\n');
+  console.log(util.inspect(result0, false, null, true));
+
+  // TODO - check balance of USDC after using state
+})();
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,11 @@
+import BigNumber from "bignumber.js";
+
 export const MAINNET_USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const MAINNET_UNISWAPV2_ROUTER = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+export const MAINNET_WETH_ADDRESS = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
+export const MAX_UINT_256_HEX = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
+export const MAX_UINT_256 = (new BigNumber(MAX_UINT_256_HEX)).toFixed(0);
 
 export const CustomLogTracer = "{\n" +
   "    data: [],\n" +
